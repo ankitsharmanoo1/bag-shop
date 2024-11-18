37cc40bf22b867fbb7bfb1011f69b204c89124b8
Ankit Sharma <ankitsharma11876@gmail.com>
2024-11-18 16:00:07 +0530

    creating user modules

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,31 @@
 const express = require("express");
+const userModels = require("../models/user-models");
 const router = express.Router();
 
+
+
+
 router.get("/", (req,res)=>{
-     res.send("hello From Users")
+     res.send("Owner is working")
 })
+
+
+
+router.post("/register", async(req,res)=>{
+    try{
+     let {email,password,fullname} = req.body;
+     let user = await userModels.create({
+          email,
+          password,
+          fullname
+     })
+     res.send(user)
+    }
+    catch(err){
+     console.log(err.message);
+     
+    }
+})
+
 
 module.exports = router;
